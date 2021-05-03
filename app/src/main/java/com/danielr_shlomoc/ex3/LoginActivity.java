069bbf4d114bfa21b9ccb955a10f545a38f4c44d
Daniel-Raz <dany6735@gmail.com>
2021-05-03 15:49:05 +0300

    make database and users table + handle login& logout

--- app/src/main/java/com/danielr_shlomoc/ex3/LoginActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex3/LoginActivity.java
@@ -1,15 +1,166 @@
 package com.danielr_shlomoc.ex3;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String MY_DB_NAME = "TodosDB.db";
+    private SQLiteDatabase todosDB = null;
+    private Button loginBtn;
+    private EditText userName, userPassword;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        int a;
+
+        setTitle("Todo Login");
+        Log.d("mylog", "onCreate()\n");
+
+        try {
+            // Opens a current database or creates it
+            // Pass the database name, designate that only this app can use it
+            // and a DatabaseErrorHandler in the case of database corruption
+            todosDB = openOrCreateDatabase(MY_DB_NAME, MODE_PRIVATE, null);
+
+            // build an SQL statement to create 'users' table (if not exists)
+            String sql = "CREATE TABLE IF NOT EXISTS users (username VARCHAR primary key, password VARCHAR);";
+            todosDB.execSQL(sql);
+            sql = "CREATE TABLE IF NOT EXISTS todos (id integer primary key , username VARCHAR , title VARCHAR, description VARCHAR, datatime LONG);";
+            todosDB.execSQL(sql);
+        } catch (Exception e) {
+            Log.d("debug", "Error Creating Database");
+        }
+
+        loginBtn = findViewById(R.id.loginBtnID);
+        loginBtn.setOnClickListener(this);
+        userName = findViewById(R.id.userNameID);
+        userPassword = findViewById(R.id.userPasswordID);
+
+        sp = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBtnID:
+                authentication();
+                break;
+
+        }
+    }
+
+    private void authentication() {
+
+        String userName = this.userName.getText().toString();
+        String userPassword = this.userPassword.getText().toString();
+        boolean userExist = false;
+
+        String query = "SELECT username, password FROM users";
+        Cursor cr = todosDB.rawQuery(query, null);
+        String password = "";
+
+        int usernameColumn = cr.getColumnIndex("username");
+        int passwordColumn = cr.getColumnIndex("password");
+
+        // missing one or more details.
+        if (userPassword.length() == 0 || userName.length() == 0) {
+            Toast.makeText(this, "One or more details missing", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (cr.moveToFirst()) {
+            do {
+
+                String name = cr.getString(usernameColumn);
+
+                // user exist check the password
+                if (userName.equals(name)) {
+                    Log.d("mylog", "user exist");
+                    userExist = true;
+                    password = cr.getString(passwordColumn);
+
+                    if (userPassword.equals(password))
+                        login();
+                    else
+                        Toast.makeText(this, "Wrong password please try again", Toast.LENGTH_LONG).show();
+
+                    break;
+                }
+
+            } while (cr.moveToNext());
+            cr.close();
+        }
+
+        // add new user
+        if (!userExist) {
+            addUser(userName, userPassword);
+            login();
+        }
+
+    }
+
+    //This function login and move to ToDoListActivity
+    private void login() {
+        Intent toDoListActivity = new Intent(this, ToDoListActivity.class);
+        startActivity(toDoListActivity);
+    }
+
+    //This function add new user to database
+    private void addUser(String userName, String userPassword) {
+        String addUser = "INSERT INTO users (username, password) VALUES ('" + userName + "', '" + userPassword + "');";
+        todosDB.execSQL(addUser);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("mylog", "onStart()\n");// check if already user loges in
+//        if (sp.getString("user", null) != null)
+//            login();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("mylog", "onStart()\n");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("mylog", "onPause()\n");
+        String temp = this.userName.getText().toString();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("user", temp);
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("mylog", "onResume()\n");
+
     }
 }
--- app/src/main/java/com/danielr_shlomoc/ex3/ToDoListActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex3/ToDoListActivity.java
@@ -2,13 +2,89 @@ package com.danielr_shlomoc.ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ToDoListActivity extends AppCompatActivity {
+
+    public static final String MY_DB_NAME = "TodosDB.db";
+    private SQLiteDatabase todosDB = null;
+    private SharedPreferences sp;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+
+        try {
+            // Opens a current database or creates it
+            // Pass the database name, designate that only this app can use it
+            // and a DatabaseErrorHandler in the case of database corruption
+            todosDB = openOrCreateDatabase(MY_DB_NAME, MODE_PRIVATE, null);
+
+        } catch (Exception e) {
+            Log.d("debug", "Error Creating Database");
+        }
+
+
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.logoutbtn, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logoutBtnID) {
+            // do something here
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //This function logout and move to LoginActivity
+    private void logout() {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sp = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        username = sp.getString("user",null);
+
+        setTitle("Todo List ("+username+")");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("user", null);
+        editor.commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+
     }
 }
