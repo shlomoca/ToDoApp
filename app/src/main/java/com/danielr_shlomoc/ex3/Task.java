6411cd4ebdbc64828e79520f7182e7787bd02077
Daniel-Raz <dany6735@gmail.com>
2021-05-09 14:27:54 +0300

    add documentation

--- app/src/main/java/com/danielr_shlomoc/ex3/Task.java
+++ app/src/main/java/com/danielr_shlomoc/ex3/Task.java
@@ -14,9 +14,7 @@ public class Task {
 
 
     public Task(String title, String description, String date, String time, int id) throws IllegalArgumentException {
-//        FORMATTER = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH);
-//        DATE_F = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
-//        TIME_F = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+
         validateInput(title, description, date, time);
         this.title = title;
         this.description = description;
@@ -25,12 +23,9 @@ public class Task {
         this.id = id;
 
     }
+
     public Task(int id) throws IllegalArgumentException {
-//        FORMATTER = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.ENGLISH);
-//        DATE_F = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
-//        TIME_F = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
         loadTaskFromDB(id);
-//        this.id = id;
     }
 
     private void loadTaskFromDB(int id) {
